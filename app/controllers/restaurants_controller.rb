3e93581c21d7a76347f8d0a6cc222d9d81a23f66
Laurent Treuil <laurent.treuil@gmail.com>
2020-02-12 16:42:28 +0100

    to be continued

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,27 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
-  def show
-    @restaurants = Restaurant.find(params[:id])
-  end
+
   def new
     @restaurant = Restaurant.new
   end
+
   def create
-    @restaurant = Restaurant.new(params[:restaurant])
-    @restaurant.save
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+      else
+      render :new
+    end
+  end
+
+   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
